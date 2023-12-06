6efad5bd5657f8dfb77228eb62262a91a1c277fc
Natan16 <natanvianat16@gmail.com>
2023-12-06 11:36:44 -0300

    Refactors middleware test

--- tests/test_middlewares.py
+++ tests/test_middlewares.py
@@ -12,12 +12,16 @@ def test_request_route_middleware():
     request.resolver_match.route.route = "/hello"
     request.resolver_match.route.view_name = "hello_world"
 
-    with mock.patch("django_query_prefixer.middlewares.sql_prefixes") as mock_sql_prefixes:
+    with mock.patch("django_query_prefixer.middlewares.set_prefix") as mock_set_prefix, 
+        mock.patch("django_query_prefixer.middlewares.remove_prefix") as mock_remove_prefix:
         assert middleware(request) == response
 
-    mock_sql_prefixes.assert_called_with(
-        view_name="hello_world",
-        route="/hello",
-    )
+    assert mock_set_prefix.call_args_list[0].kwargs == {"key": "view_name", "value": "hello_world"}
+    assert mock_set_prefix.call_args_list[1].kwargs == {"key": "route", "value": "/hello"}
+    assert mock_remove_prefix.call_args_list[0].args[0] == "view_name"
+    assert mock_remove_prefix.call_args_list[1].args[0] == "route"
+    
+
+
 
     get_response.assert_called_once_with(request)
