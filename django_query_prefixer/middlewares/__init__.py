787050a08aa0f95bd64f46ef161fea5c23333fb1
Natan16 <natanvianat16@gmail.com>
2023-12-06 10:42:19 -0300

    Adds prefixes to current context after route resolution

--- django_query_prefixer/middlewares/__init__.py
+++ django_query_prefixer/middlewares/__init__.py
@@ -1,21 +1,28 @@
 from django.urls import resolve
-from django_query_prefixer import sql_prefixes
+from django_query_prefixer import set_prefix, remove_prefix
+
+
+class RequestRouteMiddleware:
+    def  __init__(self, get_response):
+        self.get_response = get_response 
+
+    def __call__(self, request):
+        response = request.get_response(request)
+        remove_prefix("view_name")
+        remove_prefix("route")
+        return response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        set_prefix(key="view_name", value=f"{view_func.__module__}.{view_func.__name__}")
+        set_prefix(
+            key="route",
+            value=escape_comment_markers(request.resolver_match.route.route)
+        )
+        return view_func
 
 
 def request_route(get_response):
-    def middleware(request):
-        if request.resolver_match is not None:
-            route = request.resolver_match.route
-        else:
-            route = resolve(request.path_info)
-
-        with sql_prefixes(
-            view_name=route.view_name,
-            route=escape_comment_markers(route.route),
-        ):
-            return get_response(request)
-
-    return middleware
+    return RequestRouteMiddleware(get_response)
 
 
 def escape_comment_markers(value):
